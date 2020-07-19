c2b24eb063d5e85ee5a341c731d4e1ca940d7c97
Vokhmintseva <lycaste@mail.ru>
2020-07-20 01:21:10 +0300

    g

--- index.php
+++ index.php
@@ -8,11 +8,12 @@ $result = $telegram -> getWebhookUpdates(); //Передаем в перемен
 
 $text = $result["message"]["text"]; //Текст сообщения
 $chat_id = $result["message"]["chat"]["id"]; //Уникальный идентификатор пользователя
-$name = $result["message"]["from"]["username"];
+
 $keyboard = [[['text'=>"Отправить геолокацию", 'request_location'=>true]]]; //Клавиатура
 $reply = "Добро пожаловать в бота!";
-
-$reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => false ]);
+//$telegram->sendMessage()
+$reply_markup = [ 'keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => false ];
+$reply_markup = json_encode($reply_markup);
 $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup ]);
 
 //$Api = new Api('')
