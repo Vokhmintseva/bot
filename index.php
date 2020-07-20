3871b3ed5b0f69358ead9875bbe5f3f752a3505a
Vokhmintseva <lycaste@mail.ru>
2020-07-20 23:53:51 +0300

    i

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-//include __DIR__ . "/src/common.inc.php";
+include __DIR__ . "/src/common.inc.php";
 include('vendor/autoload.php');
 use Telegram\Bot\Api;
 
@@ -9,38 +9,27 @@ $result = $telegram -> getWebhookUpdates(); //Передаем в перемен
 $text = $result["message"]["text"]; //Текст сообщения
 $chat_id = $result["message"]["chat"]["id"]; //Уникальный идентификатор пользователя
 
-$keyboard = [[['text'=>"Отправить геолокацию", 'request_location'=>true]]]; //Клавиатура
-$reply = "Добро пожаловать в бота!";
+$keyboard = [['USD RUB'],['EUR RUB']]; //Клавиатура
+$reply = "Добро пожаловать в бота! Укажите буквенные коды валютной пары через пробел";
 //$telegram->sendMessage()
 $reply_markup = [ 'keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => false ];
 $reply_markup = json_encode($reply_markup);
 $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup ]);
 
-//$Api = new Api('')
-// API ключ
+/*$apiKey = "4207695fb8eeab4720b1";
+$currency1 = 'USD';
+$currency2 = 'EUR';
+function convertCurrency($amount, $from_currency, $to_currency)
+{
+    $apikey = '4207695fb8eeab4720b1';
+    $from_Currency = urlencode($from_currency);
+    $to_Currency = urlencode($to_currency);
+    $query =  "{$from_Currency}_{$to_Currency}";
+    $json = file_get_contents("https://free.currconv.com/api/v7/convert?q={$query}&compact=ultra&apiKey={$apikey}");
+    $obj = json_decode($json, true);
+    $val = floatval($obj["$query"]);
+    $total = $val * $amount;
+    return number_format($total, 2, '.', '');
+}
+echo convertCurrency(1, 'USD', 'RUB');*/
 
-
-
-/*$apiKey = "b1f2de21066ee19280abfdbdcf6ce24d";
-$lat = 55.75;
-$lon = 37.62;
-$url = "http://api.openweathermap.org/data/2.5/weather?lat=" . $lat . "&lon=" . $lon . "&lang=ru&units=metric&appid=" . $apiKey;
-//$url = "http://api.openweathermap.org/data/2.5/weather?q=" . $city . "&lang=ru&units=metric&appid=" . $apiKey;
-
-$contents = file_get_contents($url);
-$weather = json_decode($contents);
-$temp_now = $weather->main->temp;
-$icon=$weather->weather[0]->icon;
-//$today = date("j.m.Y, H:i");
-$cityname = $weather->name;
-$humidity = $weather->main->humidity;
-$description = $weather->weather[0]->description;
-$src = "https://openweathermap.org/img/wn/" . $icon . "@2x.png";
-
-echo
-    $cityname."<br />".
-    "Влажность воздуха " . $humidity."%<br />".
-    $temp_now."°C<br />".$description . "
-<img src='$src' class='condition'/>";*/
-
-?>
