aded8e26de72cab3383f6351f4ddfc42de8ce8bd
Vokhmintseva <lycaste@mail.ru>
2020-07-21 19:54:09 +0300

    i

--- index.php
+++ index.php
@@ -2,21 +2,67 @@
 include __DIR__ . "/src/common.inc.php";
 include('vendor/autoload.php');
 use Telegram\Bot\Api;
-$currencyArray = ['AUD', 'AZN', 'AMD', 'BYN', 'BGN', 'BRL', 'HUF', 'KRW', 'HKD', 'DKK', 'USD', 'EUR', 'RUB', 'INR',
+$currencies = ['AUD', 'AZN', 'AMD', 'BYN', 'BGN', 'BRL', 'HUF', 'KRW', 'HKD', 'DKK', 'USD', 'EUR', 'RUB', 'INR',
     'KZT', 'CAD', 'KGS', 'CNY', 'MDL', 'TMT', 'NOK', 'PLN', 'RON', 'XDR', 'SGD', 'TJS', 'TRY', 'UZS', 'UAH', 'GBP',
 'CZK', 'SEK', 'CHF', 'ZAR', 'JPY'];
 
-function convertCurrency($amount, $from_currency, $to_currency)
+function convertCurrency($amount, $fromCurrency, $toCurrency)
 {
     $apikey = '4207695fb8eeab4720b1';
-    $from_Currency = urlencode($from_currency);
-    $to_Currency = urlencode($to_currency);
-    $query =  "{$from_Currency}_{$to_Currency}";
+    $query =  urlencode("{$fromCurrency}_{$toCurrency}");
     $json = file_get_contents("https://free.currconv.com/api/v7/convert?q={$query}&compact=ultra&apiKey={$apikey}");
     $obj = json_decode($json, true);
-    $val = floatval($obj["$query"]);
+    $val = (float)($obj["$query"]);
     $total = $val * $amount;
     return number_format($total, 2, '.', '');
+}
+
+function generatePairKeyBoard($selectedCurrency, $currencies)
+{
+    $keyBoard = [];
+    $keyRow = [];
+    $counter = 0;
+    foreach($currencies as $value)
+    {
+        if ($selectedCurrency == $value)
+        {
+            continue;
+        }
+        $keyRow[] = $selectedCurrency . "_" . $value;
+        $counter++;
+        if ($counter % 4 == 0)
+        {
+            $keyBoard[] = $keyRow;
+            $keyRow = [];
+        }
+    }
+    if (!empty($keyRow))
+    {
+        $keyBoard[] = $keyRow;
+    }
+    return $keyBoard;
+}
+
+function generateKeyBoard($currencies)
+{
+    $keyBoard = [];
+    $keyRow = [];
+    $counter = 0;
+    foreach($currencies as $value)
+    {
+        $keyRow[] = $value;
+        $counter++;
+        if ($counter % 4 == 0)
+        {
+            $keyBoard[] = $keyRow;
+            $keyRow = [];
+        }
+    }
+    if (!empty($keyRow))
+    {
+        $keyBoard[] = $keyRow;
+    }
+    return $keyBoard;
 }
 
 $telegram = new Api('1319707453:AAGpynbcay-SL9DQXY2EZHeEH5zOQqbj3ac'); //Устанавливаем токен, полученный у BotFather
@@ -24,14 +70,15 @@ $result = $telegram -> getWebhookUpdates(); //Передаем в перемен
 
 $text = $result["message"]["text"]; //Текст сообщения
 $chat_id = $result["message"]["chat"]["id"]; //Уникальный идентификатор пользователя
-$keyboard = [['USD RUB'],['EUR RUB']]; //Клавиатура
-$reply_markup = [ 'keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => false ];
-$reply_markup = json_encode($reply_markup);
+
 if($text){
     if ($text == "/start") {
-        $reply = "Добро пожаловать в бота! Укажите буквенные коды валютной пары через пробел";
+        $reply = "Добро пожаловать в бота! Укажите буквенные коды валютной пары через пробел или выберите первый элемент валютной пары";
+        $keyboard = generateKeyBoard($currencies); //Клавиатура
+        $reply_markup = [ 'keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => false ];
+        $reply_markup = json_encode($reply_markup);
         $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup ]);
-    } elseif ($text == "USD RUB") {
+    } /*elseif ($text == "USD RUB") {
         $reply = "курс доллара США к рублю: " . convertCurrency(1, 'USD', 'RUB');
         $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup ]);
     } elseif ($text == "EUR RUB"){
@@ -42,18 +89,17 @@ if($text){
         Пример сообщения, которое Вы можете отправить боту: krw inr";
         $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup ]);
     } else {
-       /* if (preg_match('/^\w{3}\s\w{3}$/', $text))
-        {
-
-        }*/
-        $keyboard = array('inline_keyboard' => array(array('button_en', 'button_ru')));
+        $reply = "Добро пожаловать в бота! Укажите буквенные коды валютной пары через пробел или выберите первый элемент валютной пары";
+        $keyboard = generateKeyBoard($currencies); //Клавиатура
         $reply_markup = [ 'keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => false ];
         $reply_markup = json_encode($reply_markup);
-        $reply = "Добро пожаловать в бота! Укажите буквенные коды валютной пары через пробел";
         $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup ]);
-    }
+    }*/
 } else {
-    $reply = "Отправьте текстовое сообщение. Укажите буквенные коды валютной пары через пробел";
+    $reply = "Добро пожаловать в бота! Укажите буквенные коды валютной пары через пробел или выберите первый элемент валютной пары";
+    $keyboard = generateKeyBoard($currencies); //Клавиатура
+    $reply_markup = [ 'keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => false ];
+    $reply_markup = json_encode($reply_markup);
     $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup ]);
 }
 
